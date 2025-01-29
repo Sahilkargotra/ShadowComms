988d3144d509303f632f314ce0ce993fab6c7b32
Sahil Kargotra <kargotrasahil@gmail.com>
2025-01-29 17:44:27 +0530

    updated deletemsg

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,20 +4,20 @@ import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth'
 {
-    interface User{
-        _id?: string
-        isVerified ?: boolean
-        isAcceptingMessages ?: boolean
-        username ?:  string
-    }
     interface Session {
         user : {
-            _id ?: string
-            isVerified ?: boolean
-            isAcceptingMessages ?: boolean
-            username ?:  string   
-         } & DefaultSession["user"]
+            _id ?: string;
+            isVerified ?: boolean;
+            isAcceptingMessages ?: boolean;
+            username ?:  string;
+         } & DefaultSession['user'];
         }
+    interface User{
+        _id?: string;
+        isVerified ?: boolean;
+        isAcceptingMessages ?: boolean;
+        username ?:  string;
+    }
 }
 declare module 'next-auth/jwt'
 {
